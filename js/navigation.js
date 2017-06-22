768c69fa70f2f4e681f07446183e3e027f8fbad3
Tom Merchant <me@tmerchant.com>
2017-06-22 20:02:37 +0100

    Redesigned header and navigation

---
+++ js/navigation.js
@@ -0,0 +1,20 @@
+
+function update()
+{
+  if($(".navbutton:hover").length > 0)
+  {
+    $("#underline").css({
+      left: $(".navbutton:hover").offset().left + 25,
+      width: $(".navbutton:hover").width() - 50
+    });
+  }
+  else
+  {
+    $("#underline").css({
+      left: $(".selected:first").offset().left,
+      width: $(".selected:first").width()
+    });
+  }
+}
+
+window.setInterval(update, 100);
